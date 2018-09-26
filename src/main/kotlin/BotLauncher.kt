4e8379799e165658d8cb249baaa4aa40a6e2215f
Alejandro Urquieta Salazar <ales.urquieta@gmail.com>
2018-09-25 21:20:23 -0500

    Update BotLauncher.kt

--- src/main/kotlin/BotLauncher.kt
+++ src/main/kotlin/BotLauncher.kt
@@ -1,7 +1,7 @@
 object BotLauncher{
     @JvmStatic
     fun main(args: Array<String>){
-        val bot = Bot("NDkyODkxNDA1ODI0NTU3MDY2.DodDqQ.O_0uoy6zX3sVveb8WrVsVKKoSE4")
+        val bot = Bot("------")
         bot.start()
     }
 }
